44e96943eb51427f0e94e76b553fa029d3e4e0ab
SK. Saad Bin Azad Siam <sksaadbinazadsiam@gmail.com>
2025-08-13 00:23:00 +0600

    new views

---
+++ cms/cmsApp/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
+from . import forms,models
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.auth.models import Group
+from django.db.models import Q
+
+# Create your views here.
+def index(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'base.html')
+def patientclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'patientclick.html')
+
+# Signup-patient
+def patient_signup_view(request):
+    userForm=forms.PatientUserForm()
+    patientForm=forms.PatientForm()
+    mydict={'userForm':userForm,'patientForm':patientForm}
+    if request.method=='POST':
+        userForm=forms.PatientUserForm(request.POST)
+        patientForm=forms.PatientForm(request.POST,request.FILES)
+        if userForm.is_valid() and patientForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            patient=patientForm.save(commit=False)
+            patient.user=user
+            patient.assignedDoctorId=request.POST.get('assignedDoctorId')
+            patient=patient.save()
+            my_patient_group = Group.objects.get_or_create(name='PATIENT')
+            my_patient_group[0].user_set.add(user)
+        return HttpResponseRedirect('patientlogin')
+    return render(request,'patientsignup.html',context=mydict)
+
+def is_patient(user):
+    return user.groups.filter(name='PATIENT').exists()
+
+@login_required(login_url='patientlogin')
+@user_passes_test(is_patient)
+def patient_dashboard_view(request):
+    patient=models.Patient.objects.get(user_id=request.user.id)
+    doctor=models.Doctor.objects.get(user_id=patient.assignedDoctorId)
+    mydict={
+    'patient':patient,
+    'doctorName':doctor.get_name,
+    'doctorMobile':doctor.mobile,
+    'doctorAddress':doctor.address,
+    'symptoms':patient.symptoms,
+    'doctorDepartment':doctor.department,
+    'admitDate':patient.admitDate,
+    }
+    return render(request,'patient_dashboard.html',context=mydict)
+
+
+
+
